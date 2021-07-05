ae43e97cf6393248c860af6942124c949f4f7677
SeungHoon Jang <jsh1400512@naver.com>
2021-07-05 12:12:10 +0900

    init

---
+++ myhome/src/test/java/com/jsh/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsh.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
